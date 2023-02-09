d4d23ec07eb9a1a0fb41e3fee2777f4ff44d2a52
Matheus Paizante <matheus_paizante@hotmail.com>
2023-02-09 19:50:31 -0300

    Create Ex-10.js

---
+++ Js/Ex-10.js
@@ -0,0 +1,12 @@
+let idade = 65
+if (idade < 16 ){
+    console.log('Menor de idade')
+}
+else if(idade < 18 || idade > 60){
+        console.log('Voto opcional')
+
+}
+else if (idade >=18 ){
+    console.log('Voto obrigatorio.')
+
+} 
