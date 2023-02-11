8da125b973c2ee2a57da185b0eaf6a16b76232b7
Matheus Paizante <matheus_paizante@hotmail.com>
2023-02-10 22:46:40 -0300

    Update Ex-14.js

--- Js/Ex-14.js
+++ Js/Ex-14.js
@@ -2,3 +2,10 @@ let valores = [8 ,1 , 7, 4, 2 , 9]
 for (let pos = 0; pos < valores.length; pos ++){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
+
+let carro = [ 8 ,9 , 15 , 78 , 105 , 3 , 4 , 0 , 41]
+carro.sort()
+for(let p in carro){
+    
+    console.log(`A posiçãoooooooo ${p} tem o valor ${carro[p]}`)
+}
