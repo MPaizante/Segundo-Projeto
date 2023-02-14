102f16f94b0aa67d513ee40ce7fa92a1afb789b3
Matheus Paizante <matheus_paizante@hotmail.com>
2023-02-13 22:44:07 -0300

    .

--- Js/Ex-19.js
+++ Js/Ex-19.js
@@ -1,1 +1,20 @@
-alert('Olá')
+
+
+function carregar(){
+    let msg =window.document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >=0 && hora <12){
+        img.src = '../manha.jpg'
+        document.body.style.background = 'yellow'
+    } else if (hora >=12 && hora < 18){
+        img.src = '../tarde.jpg'
+        document.body.style.background = 'red'
+    }else{
+        img.src = '../noite.jpg'
+        document.body.style.background = 'gray'
+    }
+}
+
