36557116bbb3faaca0a489ab89f5f7074559e90c
Matheus Paizante <matheus_paizante@hotmail.com>
2023-02-14 23:43:28 -0300

    Update Exjs.js

--- Js/Exercicio03/Exjs.js
+++ Js/Exercicio03/Exjs.js
@@ -9,5 +9,37 @@ function verificar(){
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada: ${idade}`
+        let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >=0 && idade <10){
+                img.setAttribute('src','../imagens/bebe.jpg')
+            } else if(idade <21){
+                img.setAttribute('src','../imagens/bebe.jpg')
+            } else if(idade <50){
+                img.setAttribute('src','../imagens/bebe.jpg')
+            } else{
+
+            }
+        }   
+        else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if(idade >=0 && idade <10){
+            img.setAttribute('src','../imagens/bebe.jpg')
+
+            } else if(idade <21){
+                img.setAttribute('src','../imagens/bebe.jpg')
+            } else if(idade <50){
+            img.setAttribute('src','../imagens/bebe.jpg')
+            } else{
+            img.setAttribute('src','../imagens/bebe.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    
     }
 }
