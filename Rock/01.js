81351407eb0b496446b28287376ead579bd40d97
Matheus Paizante <matheus_paizante@hotmail.com>
2023-02-17 15:58:01 -0300

    .

---
+++ Rock/01.js
@@ -0,0 +1,17 @@
+const person = {
+    nome: 'Matheus', age: 24, weight: 88.6, isAdmin:true
+
+}
+
+console.log(person)
+
+const animais =['Leão', 'Cachorro' , 'Gato' ,
+               { nome:'Urso' , idade: 3
+
+            } 
+]
+    
+
+console.log(animais[0])
+console.log(animais.length)
+console.log(animais.name)
