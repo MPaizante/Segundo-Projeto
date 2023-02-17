aec281b5bafd553549962a200dc4c24f0943212b
Matheus Paizante <matheus_paizante@hotmail.com>
2023-02-17 17:08:40 -0300

    .

---
+++ Rock/02.js
@@ -0,0 +1,17 @@
+let Bigode = 70
+console.log(typeof Bigode)
+
+let nome = 'Matheus'
+let idade = 24
+let numero = 9.9
+let escrito = true
+
+let estuda = {
+    nome : 'Matheus',
+    idade : 24,
+    numero : 9.9,
+    escrito : true,
+}
+console.log(typeof estuda)
+console.log(estuda)
+console.log(`O ${nome} tem a idade de ${idade} anos, tirou a nota ${numero} e pesa ${Bigode}Kg.`)
